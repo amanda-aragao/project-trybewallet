de8509163d56aeedced9138bd3e311339c7c5970
Amanda Aragão <amandaaragao72@gmail.com>
2023-04-11 16:19:51 -0300

    Requisito 1 realizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    disabledButton: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.verifyInput);
+  };
+
+  verifyInput = () => {
+    const { email, password } = this.state;
+    const regexEmail = /\S+@\S+\.\S+/;
+    const testingEmail = regexEmail.test(email);
+    const numberMin = 6;
+    const testingPassword = password.length >= numberMin;
+    this.setState({ disabledButton: !(testingEmail && testingPassword) });
+  };
+
+  handleClick = () => {
+    const { history, dispatch } = this.props;
+    const { email, password } = this.state;
+    history.push('/carteira');
+    dispatch(saveLogin({ email, password }));
+  };
+
   render() {
-    return <div>Login</div>;
+    const { disabledButton, email, password } = this.state;
+    return (
+      <div>
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={ this.handleChange }
+          name="email"
+          value={ email }
+        />
+        <input
+          type="email"
+          data-testid="password-input"
+          onChange={ this.handleChange }
+          name="password"
+          value={ password }
+        />
+        <button
+          type="button"
+          disabled={ disabledButton }
+          onClick={ this.handleClick }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+export const SAVE_LOGIN = 'SAVE_LOGIN';
+export const saveLogin = (email, password) => ({
+  type: SAVE_LOGIN,
+  payload: { email, password },
+});
