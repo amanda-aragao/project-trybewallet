5ff17383ef1da905baeb63116748e78ee9a192c1
Amanda Aragão <amandaaragao72@gmail.com>
2023-04-13 02:52:10 -0300

    Requisito 6 e 7 resolvidos

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((keyOptn) => (
+              <tr key={ keyOptn.id }>
+                <td>{keyOptn.description}</td>
+                <td>{keyOptn.tag}</td>
+                <td>{keyOptn.method}</td>
+                <td>{Number(keyOptn.value).toFixed(2)}</td>
+                <td>{keyOptn.exchangeRates[keyOptn.currency].name}</td>
+                <td>{Number(keyOptn.exchangeRates[keyOptn.currency].ask).toFixed(2)}</td>
+                <td>
+                  {Number(keyOptn.value * keyOptn.exchangeRates[keyOptn.currency].ask)
+                    .toFixed(2)}
+                </td>
+                <td>Real</td>
+                <td>Editar/Excluir</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+
+  })).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
---
+++ src/tests/helpers/Wallet.test.js
@@ -0,0 +1,20 @@
+// import { screen, waitFor } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+// import { renderWithRouterAndRedux } from './helpers/renderWith';
+
+// describe('', () => {
+//   test('', () => {
+
+//   });
+
+//   test('', () => {
+
+//   });
+//   test('', () => {
+
+//   });
+//   test('', () => {
+
+//   });
+// });
